dc549ae3bf4a139d8f67be9fc6b7df3880b394d2
lemondy <lemondy9@gmail.com>
2024-02-02 19:17:04 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,11 +14,11 @@ import { seo } from '~/lib/seo'
 export const metadata: Metadata = {
   metadataBase: seo.url,
   title: {
-    template: '%s | Cali Castle',
+    template: '%s | lemondy',
     default: seo.title,
   },
   description: seo.description,
-  keywords: 'Cali,Cali Castle,郭晓楠,佐玩,创始人,CEO,开发者,设计师,细节控,创新',
+  keywords: 'lemondy,张云,,开发者,数据挖掘工程师,设计师,细节控,创新',
   themeColor: [
     { media: '(prefers-color-scheme: dark)', color: '#000212' },
     { media: '(prefers-color-scheme: light)', color: '#fafafa' },
@@ -38,13 +38,13 @@ export const metadata: Metadata = {
   openGraph: {
     title: {
       default: seo.title,
-      template: '%s | Cali Castle',
+      template: '%s | lemondy',
     },
     description: seo.description,
-    siteName: 'Cali Castle',
+    siteName: 'lemondy',
     locale: 'zh_CN',
     type: 'website',
-    url: 'https://cali.so',
+    url: 'https://lemondy.org',
   },
   twitter: {
     site: '@thecalicastle',
