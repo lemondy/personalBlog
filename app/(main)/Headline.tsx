4b6ef29c6055d8f60df3276aee76eabe8afa47db
lemondy <lemondy9@gmail.com>
2024-02-02 16:05:04 +0800

    Update Headline.tsx

--- app/(main)/Headline.tsx
+++ app/(main)/Headline.tsx
@@ -80,10 +80,7 @@ export function Headline() {
         }}
       >
         <Balancer>
-          我是 Lemondy，
-          <PeekabooLink href="https://zolplay.com"></PeekabooLink>
-          创始人，目前带领着佐玩致力于创造一个充满创造力的工作环境，同时鼓励团队创造影响世界的产品。
-          我热爱开发，设计，创新，享受生活，以及在未知领域中探索。
+          我是 Lemondy，欢迎关注。
         </Balancer>
       </motion.p>
       <motion.div
@@ -99,33 +96,33 @@ export function Headline() {
         }}
       >
         <SocialLink
-          href="https://cali.so/twitter"
+          href="https://lemondy.org/twitter"
           aria-label="我的推特"
           platform="twitter"
         />
         <SocialLink
-          href="https://cali.so/youtube"
+          href="https://lemondy.org/youtube"
           aria-label="我的 YouTube"
           platform="youtube"
         />
         <SocialLink
-          href="https://cali.so/bilibili"
+          href="https://lemondy.org/bilibili"
           aria-label="我的 Bilibili"
           platform="bilibili"
         />
         <SocialLink
-          href="https://cali.so/github"
+          href="https://lemondy.org/github"
           aria-label="我的 GitHub"
           platform="github"
         />
         <SocialLink
-          href="https://cali.so/tg"
+          href="https://lemondy.org/tg"
           aria-label="我的 Telegram"
           platform="telegram"
         />
         <SocialLink href="/feed.xml" platform="rss" aria-label="RSS 订阅" />
         <SocialLink
-          href="mailto:hi@cali.so"
+          href="mailto:hi@lemondy.org"
           aria-label="我的邮箱"
           platform="mail"
         />
