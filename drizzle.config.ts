266b82c83d76d455e99bb16529296d441d4134f3
lemondy <lemondy9@gmail.com>
2024-03-14 23:31:01 +0800

    Update drizzle.config.ts

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,16 +1,10 @@
-import * as dotenv from "dotenv";
-import type { Config } from "drizzle-kit";
+import * as dotenv from 'dotenv'
+import type { Config } from 'drizzle-kit'
 dotenv.config()
 
 export default {
   driver: 'pg',
   schema: './db/schema.ts',
   out: './db/migrations',
-  dbCredentials: { 
-    host: process.env.DATABASE_HOST || '',
-    port: parseInt(process.env.DATABASE_PORT || '3306'),
-    user: process.env.DATABASE_USERNAME,
-    password: process.env.DATABASE_PASSWORD,
-    database: process.env.DATABASE_NAME || '',
-  },
+  dbCredentials: { connectionString: process.env.DATABASE_URL || '' },
 } satisfies Config
